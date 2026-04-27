be419985b560ca3181199c6e57e582a1c52b3d47
EgorAsusS <egor.firsov.egor@yandex.ru>
2026-04-27 23:01:15 +0700

    decode()

--- Lab_4/main.c
+++ Lab_4/main.c
@@ -5,22 +5,25 @@
 #include <string.h>
 
 FILE* encode(FILE* file, char* file_new_name);
+FILE* decode(FILE* file, char* file_new_name);
 
 int main() {
     FILE* file = fopen("data.txt", "r");
     encode(file, "encrypt.txt");
     fclose(file);
+    file = fopen("encrypt.txt", "r");
+    decode(file, "data.txt");
+    fclose(file);
     return 0;
 }
 
 
-FILE* encode(FILE* file, char* file_new_name) {
+FILE* encode(FILE* file, char* filepath) {
     FILE* file_new = NULL;
     if (file) {
-        file_new = fopen(file_new_name, "w");
+        file_new = fopen(filepath, "w");
         char el[8] = { 0 };
         int ix = 0;
-        unsigned char ___________________el = 0;
         char _el = 0;
         int flag = 0;
         unsigned char mask = 1;
@@ -40,11 +43,59 @@ FILE* encode(FILE* file, char* file_new_name) {
                     }
                 }
                 else {
-                    for (int i = 1, j = 7; i < 8 && flag != EOF; i++, j--) {
+                    for (int i = 1, j = 7; i < 8; i++, j--) {
                         _el = ((mask & el[0]) << j) | el[i];
-                        ___________________el = _el;
                         mask = mask << 1;
                         fprintf(file_new, "%c", _el);
+                    }
+                }
+
+            }
+            fclose(file_new);
+        }
+    }
+    return file_new;
+}
+
+FILE* decode(FILE* file, char* filepath) {
+    FILE* file_new = NULL;
+    if (file) {
+        file_new = fopen(filepath, "w");
+        char el[7] = { 0 };
+        int ix = 0;
+        char _el = 0;
+        bool flag_end = 1;
+        int flag = 0;
+        unsigned char mask = 0;
+        unsigned char _mask = 0;
+        if (file_new) {
+            while (flag != EOF) {
+                mask = 128;
+                _mask = ~mask;
+                ix = 0;
+                flag_end = 1;
+                while (ix < 7 && flag != EOF) {
+                    flag = fscanf(file, "%c", &el[ix]);
+                    if (flag != EOF) {
+                        if (mask & el[ix]) {
+                            flag_end = 0;
+                        }
+                        ix++;
+                    }
+                }
+                if (ix < 7 || flag_end) {
+                    for (int i = 0; i < ix; i++) {
+                        fprintf(file_new, "%c", el[i]);
+                    }
+                }
+                else {
+                    for (int i = 0, j = 7; i < 7; i++, j--) {
+                        _el = ((mask & el[i]) >> j) | _el;
+                        el[i] = el[i] & _mask;
+                    }
+                    fprintf(file_new, "%c", _el);
+                    for (int i = 0; i < 7; i++) {
+                        fprintf(file_new, "%c", el[i]);
                     }
                 }
 
